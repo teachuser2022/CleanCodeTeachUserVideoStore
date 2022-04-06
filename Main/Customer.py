444f8b42d5a3dc2b74f4a596324c5c96af3fbf30
mohsen jalalian <jalalianmohse89@gmail.com>
2022-04-06 13:47:09 +0430

    refactoring test

--- Main/Customer.py
+++ Main/Customer.py
@@ -11,6 +11,8 @@ class Customer:
     def __init__(self, name):
         self.name = name
         self.rentals=[]
+        self.totalAmount = 0
+        self.frequentRenterPoints = 0
 
     # ----------------------------------------------------------------------------------------------------------
 
@@ -25,8 +27,8 @@ class Customer:
     # ----------------------------------------------------------------------------------------------------------
 
     def statement(self):
-        _totalAmount = 0
-        _frequentRenterPoints = 0
+        self.totalAmount = 0
+        self.frequentRenterPoints = 0
         _result= "Rental Record for " + self.getName() + "\n"
         for rental in self.rentals:
             _thisAmount = 0
@@ -42,14 +44,20 @@ class Customer:
                 if rental.getDaysRented() > 3:
                     _thisAmount += (rental.getDaysRented() - 3) * 1.5
 
-            _frequentRenterPoints += 1
+            self.frequentRenterPoints += 1
             if rental.getMovie().getPriceCode() == Movie.NEW_RELEASE and rental.getDaysRented() > 1:
-                _frequentRenterPoints += 1
+                self.frequentRenterPoints += 1
             _result+= "\t" + rental.getMovie().getTitle() + "\t" + "{:.1f}".format(_thisAmount) + "\n"
-            _totalAmount += _thisAmount
+            self.totalAmount += _thisAmount
 
-        _result+= "You owed " + "{:.1f}".format(_totalAmount) + "\n"
-        _result+= "You earned " + str(_frequentRenterPoints) + " frequent renter points\n"
+        _result+= "You owed " + "{:.1f}".format(self.totalAmount) + "\n"
+        _result+= "You earned " + str(self.frequentRenterPoints) + " frequent renter points\n"
 
         return _result
     # ----------------------------------------------------------------------------------------------------------
+
+    def getTotalAmount(self):
+        return self.totalAmount
+
+    def setFrequentRenterPoints(self):
+        return self.frequentRenterPoints
